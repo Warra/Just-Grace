be4ff759e3e7f9f1007c1f204f835c3bd57605e5
Warra <warrenmanley@gmail.com>
2016-03-26 10:05:28 +0200

    Updates footer to 2016

--- wp-content/themes/just-grace/footer.php
+++ wp-content/themes/just-grace/footer.php
@@ -2,20 +2,18 @@
         	<div class="container">
             	<div class="innert-links">
                 <?php dynamic_sidebar('footer-1'); ?>
-                    
                 </div>
                 <div class="footer-seprator"></div>
                 <div class="footer-contact-sec">
                 <?php dynamic_sidebar('footer-2'); ?>
-                    
                 </div>
             </div>
         </div>
         <div class="container">
         	<div class="privacy-sec">
-            	Just Grace © 2015
+            	Just Grace © 2016
             </div>
         </div>
-          <?php wp_footer(); ?> 
+          <?php wp_footer(); ?>
   </body>
 </html>
