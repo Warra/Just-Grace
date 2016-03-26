5a594d96cab1396d5dc3421ac8f1953a8a95f89a
Warra <warrenmanley@gmail.com>
2016-03-26 11:39:58 +0200

    Removes green box of text from home-page

--- wp-content/themes/just-grace/index.php
+++ wp-content/themes/just-grace/index.php
@@ -1,30 +1,26 @@
-<?php 
+<?php
 /*
-*
+*   Template name: Home Page
 *
 *
 */
-get_header(); 
-
+get_header();
 ?>
-
 <div class="content-area">
     <div class="inner-content-bg about-content-bg">
-    	<div class="container">
-            	<div class="content-left-side">
-                	<?php dynamic_sidebar('inner-content-left'); ?>
+        <div class="container">
+                <div class="content-left-side">
+                    <?php dynamic_sidebar('inner-content-left'); ?>
                 </div>
                 <div class="content-right-side">
-                	<div class="right-green-1">
-                    <?php dynamic_sidebar('inner-content-right'); ?>
-                    	
+                    <div  style="height:250px"class="right-green-1">
+
                     </div>
                     <div class="right-green-2">
                     <?php while ( have_posts() ) : the_post(); ?>
-					   <?php the_content(); ?>
+                       <?php the_content(); ?>
                          <?php /*?> <?php comments_template( '', true ); ?><?php */?>
                       <?php endwhile; // end of the loop. ?>
-                    	
                     </div>
                 </div>
                 <div class="clear"></div>
@@ -32,11 +28,11 @@ get_header();
         </div>
      </div>
      <div class="footer-1">
-        	<div class="container">
-            	<div class="footer-1-left">
+            <div class="container">
+                <div class="footer-1-left">
                 <?php dynamic_sidebar('footer-top-1'); ?>
-                	
+
                 </div>
             </div>
-        </div>   
-        <?php get_footer(); ?> 
+        </div>
+        <?php get_footer(); ?>
